ffb0e3c06ce2783213e262d9ded559f5e7ca60f0
WITCHERBLADE <aryannirwal72@gmail.com>
2025-07-09 21:28:17 +0530

    make the service section dynamic with different services wip ,M&E Engineering UI done

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,33 @@
-import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Layout from './components/layout/Layout.jsx';  
-import Home from './pages/home/Home.jsx';
-import MeetOurTeam from './pages/aboutUs/MeetOurTeam.jsx';
-import AccreditationMembership from './pages/aboutUs/AccreditationMembership.jsx';
-import Testimonials from './pages/aboutUs/Testimonials.jsx';
-import Careers from './pages/aboutUs/Careers.jsx';
-import SmartDesign from './pages/services/SmartDesign.jsx';
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/layout/Layout.jsx";
+import Home from "./pages/home/Home.jsx";
+import MeetOurTeam from "./pages/aboutUs/MeetOurTeam.jsx";
+import AccreditationMembership from "./pages/aboutUs/AccreditationMembership.jsx";
+import Testimonials from "./pages/aboutUs/Testimonials.jsx";
+import Careers from "./pages/aboutUs/Careers.jsx";
+import SmartDesign from "./pages/services/SmartDesign.jsx";
+import ServicePage from "./pages/services/ServicePage.jsx";
 
 function App() {
-  const router = createBrowserRouter(
-    [
-      {
-        path: '/',
-        element: <Layout />,
-        children: [
-          { path: '/', element: <Home /> },
-          { path: '/aboutus/meetourteam', element: <MeetOurTeam /> },
-          { path: '/aboutus/accreditation_Membership', element: <AccreditationMembership/> },
-          { path: '/aboutus/careers', element: <Careers/> },
-          { path: '/aboutus/testimonials', element: <Testimonials/> },
-          { path: '/services/consultancy_smartdesign', element: <SmartDesign/> },
-        ],
-      },
-    ],
-    
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/aboutus/meetourteam", element: <MeetOurTeam /> },
+        {
+          path: "/aboutus/accreditation_Membership",
+          element: <AccreditationMembership />,
+        },
+        { path: "/aboutus/careers", element: <Careers /> },
+        { path: "/aboutus/testimonials", element: <Testimonials /> },
+        // { path: '/services/consultancy_smartdesign', element: <SmartDesign/> },
+        { path: "/services/:serviceName", element: <ServicePage /> },
+      ],
+    },
+  ]);
 
   return <RouterProvider router={router} />;
 }
